97eba926f6f66b2708d217e5f019812ae3013214
blackzoro <diy_sunny@yahoo.co.jp>
2016-09-09 09:10:21 +0900

    test02

--- tcp_client1_02_process_check.sh
+++ tcp_client1_02_process_check.sh
@@ -3,7 +3,8 @@ while true
 do
 #isAlive=`ps -ef | grep " sudo python /home/pi/raspberryPiTCPClient/tcp_client1_02.py " | \grep -v grep | wc -l`
 #if [ $isAlive = 1 ]:then
-if ps axww | grep "[s]udo python /home/pi/raspberryPiTCPClient/tcp_client1_02.py" >/dev/null
+#if ps axww | grep "[s]udo python /home/pi/raspberryPiTCPClient/tcp_client1_02.py" >/dev/null
+if ps axww | grep "[p]ython /home/pi/raspberryPiTCPClient/tcp_client1_02.py" >/dev/null
 then
 echo "プロセスは生きています"
 else
