22da4122273ece4224806ba524fb3322154ba7db
KenjiMaehara <kmaehara@mel.co.jp>
2016-09-11 19:02:57 +0900

    add files
    add files

---
+++ Untitled.py
@@ -0,0 +1,46 @@
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.button import Label,Button
+from kivy.config import Config
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.textinput import TextInput
+from kivy.clock import Clock
+from datetime import datetime
+
+class RootWidget(BoxLayout):
+    def __init__(self, **kwargs):
+        super(RootWidget, self).__init__(padding=30, orientation='vertical')
+
+        self.label=Label(text="Time Display")
+        self.add_widget(self.label)
+
+        Clock.schedule_interval(self.TimerCallback, 1.0)
+
+        #self.textinput = TextInput(text='Hello world', multiline=False)
+        #self.add_widget(self.textinput)
+
+    def TimerCallback(self,dt):
+        time=datetime.now().strftime("%Y/%m/%d %H:%M:%S")
+        #self.textinput.text=time
+        self.label.text=time
+
+class TestApp(App):
+    def build(self):
+        self.root = RootWidget()
+        self.title = 'Text Input Sample'
+        return self.root
+
+TestApp().run()
+
+
+#class HelloApp(App):
+
+#    def build(self):
+#        return Label(text='Hello World')
+
+
+#if __name__ == '__main__':
+#    HelloApp().run()
+
