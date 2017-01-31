c01cc22258c3e5b70a2c5be6b8b80493396a6cce
blackzoro <diy_sunny@yahoo.co.jp>
2017-01-31 11:13:03 +0900

    0131 test005
    0131 test005

--- testSpi161101/test20170130.py
+++ testSpi161101/test20170130.py
@@ -26,59 +26,12 @@ print "writing data"
 #hello spi (ASCII)
 #data = [0x4d,0x45,0x2c,0x72,0x64,0x2c,0x01,0x0d]
 data = b"ME,rd,1\n"
-#data = "abcdefg"
-#data = [0x20]
-#resp = spi.xfer2(data)
+
 str(spi.xfer2(data))
 
-
-#data = [0x72]
-#str(spi.xfer2(data))
-#time.sleep(0.1)
-#data = [0x64]
-#str(spi.xfer2(data))
-#time.sleep(0.1)
-#data = [0x2c]
-#str(spi.xfer2(data))
-#time.sleep(0.1)
-#data = [0x01]
-#str(spi.xfer2(data))
-#time.sleep(0.1)
-#data = [0x0d]
-#str(spi.xfer2(data))
-#time.sleep(0.1)
-
-#str(spi.xfer2(data))
-#spi.xfer2(data)
-#spi.xfer2(data)
 time.sleep(0.1)
 #data = [0x16]
-#str(spi.xfer2(data))
-#time.sleep(0.1)
-#data = [0x00]
-#data = [0x0d]
-#str(spi.xfer2(data))
-#data = [0x00]
-#print "CH18  >>>" + str(spi.xfer2(data))
-#data = [0x00]
-        #print ">>>" + str(spi.xfer2([0x00]))
 
-        #time.sleep(0.1)
-        #n = 0
-        # while n < 10:
-        #         print ">>>" + str(spi.xfer2([0x00]))
-        #         n += 1
-        #else:
-        #         print "END"
-
-        #time.sleep(1)
-        #counter += 1
-        #if counter > 4:
-            #break
-        time.sleep(0.1)
-    #except(keyboardInterrupt, SystemExit):
-        #spi.close()
-        #raise
 #spi.close()
 print "done"
 
