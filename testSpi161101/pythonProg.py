c8cdf3b211e4de8c020ee271c705cde163094a96
KenjiMaehara <kmaehara@mel.co.jp>
2017-02-05 08:52:49 +0900

    test4

--- testSpi161101/pythonProg.py
+++ testSpi161101/pythonProg.py
@@ -4,6 +4,7 @@ parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-v", "--verbose", action="store_true")
 group.add_argument("-q", "--quiet", action="store_true")
+group.add_argument("-s", "--sum", action="store_true")
 parser.add_argument("x", type=int, help="the base")
 parser.add_argument("y", type=int, help="the exponent")
 parser.add_argument("z", type=int, help="the exponent")
@@ -16,8 +17,13 @@ if args.quiet:
 	print answer
 elif args.verbose:
 	print "{} to the power {} equals {}".format(args.x, args.y, answer)
-else:
+elif args.sum:
 	print args.x
 	print args.y
 	print args.z
+	print "{}+{}+{} == {}".format(args.x, args.y, args.z, answer)
+	#print "{} to the power {} equals {}".format(args.x, args.y, answer)
+else:
+	print args.x
+	print args.y
 	print "{}+{}+{} == {}".format(args.x, args.y, args.z, answer)
