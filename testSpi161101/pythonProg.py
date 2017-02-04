6a70258c30fd30e8906d083343056d6999ddaea1
KenjiMaehara <kmaehara@mel.co.jp>
2017-02-05 00:11:38 +0900

    test3

--- testSpi161101/pythonProg.py
+++ testSpi161101/pythonProg.py
@@ -6,8 +6,9 @@ group.add_argument("-v", "--verbose", action="store_true")
 group.add_argument("-q", "--quiet", action="store_true")
 parser.add_argument("x", type=int, help="the base")
 parser.add_argument("y", type=int, help="the exponent")
+parser.add_argument("z", type=int, help="the exponent")
 args = parser.parse_args()
-answer = args.x**args.y
+answer = args.x+args.y+args.z
 
 if args.quiet:
 	print x
@@ -18,4 +19,5 @@ elif args.verbose:
 else:
 	print args.x
 	print args.y
-	print "{}^{} == {}".format(args.x, args.y, answer)
+	print args.z
+	print "{}+{}+{} == {}".format(args.x, args.y, args.z, answer)
