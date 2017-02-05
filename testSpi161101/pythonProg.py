e27fa81fe8fd7c7eaf18aa73baeeb68b650bf2a7
KenjiMaehara <kmaehara@mel.co.jp>
2017-02-05 21:10:52 +0900

    test10

--- testSpi161101/pythonProg.py
+++ testSpi161101/pythonProg.py
@@ -1,31 +1,11 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group()
-group.add_argument("-v", "--verbose", action="store_true")
-group.add_argument("-q", "--quiet", action="store_true")
-group.add_argument("-s", "--sum", action="store_true")
-parser.add_argument("x", type=int, help="the base")
-parser.add_argument("y", type=int, help="the exponent")
-parser.add_argument("z", type=int, help="the exponent")
-args = parser.parse_args()
-answer = args.x+args.y
-answer02 = args.x+args.y+args.z
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('integers', metavar='N', type=int, nargs='+',
+                   help='an integer for the accumulator')
+parser.add_argument('--sum', dest='accumulate', action='store_const',
+                   const=sum, default=max,
+                   help='sum the integers (default: find the max)')
 
-if args.quiet:
-	print x
-	print y
-	print answer
-elif args.verbose:
-	print "{} to the power {} equals {}".format(args.x, args.y, answer)
-elif args.sum:
-	print args.x
-	print args.y
-	print args.z
-	print "{}+{}+{} == {}".format(args.x, args.y,args.z, answer02)
-	#print "{} to the power {} equals {}".format(args.x, args.y, answer)
-else:
-	print args.x
-	print args.y
-	print args.z
-	print "{}+{}+{} == {}".format(args.x, args.y, args.z, answer02)
+args = parser.parse_args()
+print args.accumulate(args.integers)
